222c329275b4e0f95352b316e52d99494185bfd9
Gareth Pile <gareth.pile@leansystems.co.za>
2021-03-02 18:50:11 +0200

    CSV file Ingestion

--- amplify/backend/function/ingestUberPaymentFile/src/go.mod
+++ amplify/backend/function/ingestUberPaymentFile/src/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.22.0 // indirect
-	github.com/aws/aws-sdk-go v1.37.20 // indirect
+	github.com/aws/aws-sdk-go v1.37.21 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/kjk/betterguid v0.0.0-20170621091430-c442874ba63a // indirect
 )
--- amplify/backend/function/ingestUberPaymentFile/src/main.go
+++ amplify/backend/function/ingestUberPaymentFile/src/main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strconv"
+	"time"
 	"strings"
 
 	"io/ioutil"
@@ -17,6 +17,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+
 )
 
 func main() {
@@ -24,17 +25,22 @@ func main() {
 }
 
 func handler(ctx context.Context, s3Event events.S3Event) {
+	fmt.Printf("Starting handler \n")
+	
+	
 	for _, record := range s3Event.Records {
 		s3 := record.S3
 		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
 		fileContent := getDataFromS3File(s3.Bucket.Name, s3.Object.Key)
 		dataExtracted := extractData(fileContent)
 		insertIntoDynamoDB(dataExtracted, s3.Object.Key)
-		fmt.Printf("Finished")
+		fmt.Printf("Finished handler")
 	}
+
 }
 
 func getDataFromS3File(bucket string, s3File string) string {
+	fmt.Printf("Starting getDataFromS#File\n")
 	//the only writable directory in the lambda is /tmp
 	file, err := os.Create("/tmp/" + s3File)
 	if err != nil {
@@ -45,7 +51,7 @@ func getDataFromS3File(bucket string, s3File string) string {
 
 	// replace with your bucket region
 	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String("<YOUR BUCKET REGION>")},
+		Region: aws.String("eu-west-1")},
 	)
 
 	downloader := s3manager.NewDownloader(sess)
@@ -65,27 +71,43 @@ func getDataFromS3File(bucket string, s3File string) string {
 		exitErrorf("Cannot read the file", err)
 	}
 
+	fmt.Printf("Finished getDataFromFile\n")
+
 	return string(dat)
 
 }
 
 func extractData(data string) []string {
+
+	fmt.Printf("Starting extractData\n")
+
 	lines := strings.Split(data, "\n")
 
-	var names []string
+	var csvlines []string
 
 	for _, currentLine := range lines {
-		name := strings.Split(currentLine, ",")[1]
-		names = append(names, name)
+	
+		csvrow := strings.Split(currentLine,"\n")[0]
+		csvlines = append(csvlines , csvrow)
+	
 	}
 
-	return names
+	fmt.Printf("Finished extractData\n")
+
+	return csvlines
 }
 
 func insertIntoDynamoDB(dataToInsert []string, fileName string) {
+
+	fmt.Printf("Starting insertIntoDynamoDB Round 2000\n")
+
+
 	type MyDataFromS3 struct {
-		FileKey string
-		Name    string
+		Id string
+		UberDriverId string
+		FirstName    string
+		LastName    string
+		Total    string
 	}
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -95,9 +117,30 @@ func insertIntoDynamoDB(dataToInsert []string, fileName string) {
 	svc := dynamodb.New(sess)
 
 	for currentIndex, currentItem := range dataToInsert {
+        
+		guid := time.Now()
+		sguid := guid.String()
+
+		stringcurrentIndex := string(currentIndex)
+		fmt.Printf(stringcurrentIndex) 
+
+		uberDriverId := strings.Split(currentItem,",")[0]
+		firstName := strings.Split(currentItem,",")[1]
+		lastName := strings.Split(currentItem,",")[2]
+		total := strings.Split(currentItem,",")[3]
+
+		uberDriverId = uberDriverId[1 : len(uberDriverId)-1]
+		firstName = firstName[1 : len(firstName)-1]
+		lastName = lastName[1 : len(lastName)-1]
+		total = total[1 : len(total)-1]
+
 		data := MyDataFromS3{
-			Name:    currentItem,
-			FileKey: fileName + "-" + strconv.Itoa(currentIndex),
+
+			Id: sguid,
+			UberDriverId: uberDriverId,
+			FirstName: firstName,
+            LastName: lastName,
+			Total: total,
 		}
 
 		av, err := dynamodbattribute.MarshalMap(data)
@@ -105,7 +148,7 @@ func insertIntoDynamoDB(dataToInsert []string, fileName string) {
 			exitErrorf("Got error marshalling new movie item:", av, err)
 		}
 
-		tableName := "MyDataFromS3"
+		tableName := "UberPaymentTransactions"
 		input := &dynamodb.PutItemInput{
 			Item:      av,
 			TableName: aws.String(tableName),
@@ -116,6 +159,8 @@ func insertIntoDynamoDB(dataToInsert []string, fileName string) {
 			exitErrorf("Got error calling PutItem:", err)
 		}
 	}
+
+	fmt.Printf("Finished insertIntoDynamoDB\n")
 }
 
 func exitErrorf(msg string, args ...interface{}) {
